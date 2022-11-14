bd6aa3a5ce95d105c94f67180fea8ac5eadff9b5
hoanganh8815 <nhanh36@gmail.com>
2022-11-14 22:36:53 +0700

    Initial commit

---
+++ src/app/Components/People/people.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html'
+})
+
+export class PeopleComponent{
+  titlePeople = 'Demo component'
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { PeopleComponent } from './Components/People/people.component'
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, PeopleComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
